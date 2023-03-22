856664e3e29b2e9ccc1bb2d23426ff8ae88eb9b2
StettenFessler <stetten.fessler@gmail.com>
2023-03-22 16:55:03 -0700

    Add EmployeeService

--- src/AddEmployee.js
+++ src/AddEmployee.js
@@ -1,6 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import EmployeeService from "./services/EmployeeService";
 
 const AddEmployee = () => {
+	const [employee, setEmployee] = useState({
+		id: "",
+		firstName: "",
+		lastName: "",
+		emailId: "",
+	});
+
+	const handleChange = (e) => {
+		const val = e.target.value;
+		setEmployee({ ...employee, [e.target.name]: val });
+	};
+
+	const saveEmployee = (e) => {
+		e.preventDefault();
+		EmployeeService.saveEmployee(employee)
+			.then((response) => {
+				console.log(response);
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	};
+
 	return (
 		<div className="flex max-w-2xl shadow border-b mx-auto">
 			<div className="px-8 py-8">
@@ -13,6 +37,9 @@ const AddEmployee = () => {
 					</label>
 					<input
 						type="text"
+						name="firstName"
+						value={employee.firstName}
+						onChange={(e) => handleChange(e)}
 						className="h-10 w-96 border mt-2 px-2 py-2"></input>
 				</div>
 				<div className="items-center justify-center h-14 w-full my-4">
@@ -21,6 +48,9 @@ const AddEmployee = () => {
 					</label>
 					<input
 						type="text"
+						name="lastName"
+						value={employee.lastName}
+						onChange={(e) => handleChange(e)}
 						className="h-10 w-96 border mt-2 px-2 py-2"></input>
 				</div>
 				<div className="items-center justify-center h-14 w-full my-4">
@@ -29,14 +59,19 @@ const AddEmployee = () => {
 					</label>
 					<input
 						type="email"
+						name="emailId"
+						value={employee.emailId}
+						onChange={(e) => handleChange(e)}
 						className="h-10 w-96 border mt-2 px-2 py-2"></input>
 				</div>
 				<div className="items-center justify-center h-14 w-full my-4 space-x-4 pt-4">
-					<button className="rounded text-white font-semibold bg-green-400 hover:bg-green-700 py-2 px-6">
+					<button
+						onClick={saveEmployee}
+						className="rounded text-white font-semibold bg-green-400 hover:bg-green-700 py-2 px-6">
 						Save
 					</button>
 					<button className="rounded text-white font-semibold bg-red-400 hover:bg-red-700 py-2 px-6">
-						Save
+						Clear
 					</button>
 				</div>
 			</div>
