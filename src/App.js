ab674c365aaaabe5246911ec8e7215f53b26b34e
StettenFessler <stetten.fessler@gmail.com>
2023-03-22 18:57:39 -0700

    Add EmployeeList component - add getEmployees method to EmployeeService

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AddEmployee from "./AddEmployee";
 import "./App.css";
+import EmployeeList from "./components/EmployeeList";
 import Navbar from "./components/Navbar";
 
 function App() {
 	return (
 		<>
-			<Navbar />
-			<AddEmployee />
+			<BrowserRouter>
+				<Navbar />
+				<Routes>
+					<Route index element={<EmployeeList />}></Route>
+					<Route path="/" element={<EmployeeList />}></Route>
+					<Route
+						path="/employee-list"
+						element={<EmployeeList />}></Route>
+					<Route
+						path="/add-employee"
+						element={<AddEmployee />}></Route>
+				</Routes>
+			</BrowserRouter>
 		</>
 	);
 }
